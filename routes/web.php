c2241cf5b566d56df5aeee0a02382b18909c0978
Unknown <Jarnovogelzang@live.nl>
2018-07-07 16:50:45 +0200

    requests

--- routes/web.php
+++ routes/web.php
@@ -11,49 +11,48 @@
 |
  */
 
-Route::middleware(['auth:basic'])->group(function () {
-  Route::resource('users', 'UserController')->only(['show', 'update', 'edit']);
-  Route::resource('orders', 'OrderController')->only(['show']);
+/*Route::middleware(['auth:basic'])->group(function () {
+Route::resource('users', 'UserController')->only(['show', 'update', 'edit']);
+Route::resource('orders', 'OrderController')->only(['show']);
 
-  Route::middleware(['auth:admin'])->group(function () {
-    Route::resource([
-      'orders' => 'OrderController',
-      'users' => 'UserController',
-    ])->only([
-      'delete',
-    ]);
+Route::middleware(['auth:admin'])->group(function () {
+Route::resource([
+'orders' => 'OrderController',
+'users' => 'UserController',
+])->only([
+'delete',
+]);
 
-    Route::resource([
-      'categories' => 'CategoryController',
-      'priceranges' => 'PriceRangeController',
-      'products' => 'ProductController',
-    ])->only([
-      'create',
-      'edit',
-      'update',
-      'store',
-    ]);
+Route::resource([
+'categories' => 'CategoryController',
+'priceranges' => 'PriceRangeController',
+'products' => 'ProductController',
+])->only([
+'create',
+'edit',
+'update',
+'store',
+]);
 
-    Route::resource('orders', 'OrderController')->only(['update', 'edit']);
-    Route::resource('priceranges', 'PriceRangeController')->only(['show']);
-  });
+Route::resource('orders', 'OrderController')->only(['update', 'edit']);
+Route::resource('priceranges', 'PriceRangeController')->only(['show']);
+});
 });
 
 Route::resource([
-  'orders' => 'OrderController',
-  'users' => 'UserController',
+'orders' => 'OrderController',
+'users' => 'UserController',
 ])->only([
-  'create',
+'create',
 ]);
 
 Route::resource([
-  'categories' => 'CategoryController',
-  'products' => 'ProductController',
+'categories' => 'CategoryController',
+'products' => 'ProductController',
 ])->only([
-  'show',
+'show',
 ]);
 
 Route::get('/', 'PagesController@index');
-Route::get('/checkout', 'PagesController@checkout');
 
-Auth::routes();
+Auth::routes();*/
