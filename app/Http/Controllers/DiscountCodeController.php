9362b7e0da47456f178da17faf6f1ddeccc15587
Unknown <Jarnovogelzang@live.nl>
2018-07-13 15:14:05 +0200

    discountcode

---
+++ app/Http/Controllers/DiscountCodeController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DiscountCodeController extends Controller {
+  /**
+   * @param CreateDiscountCodeRequest $objRequest
+   */
+  public function create(CreateDiscountCodeRequest $objRequest) {
+    Log::info('Creating an new DiscountCode.');
+
+    return view('discountcodes.create');
+  }
+
+  /**
+   * @param DestroyDiscountCodeRequest $objRequest
+   * @param DiscountCode $objDiscountCode
+   */
+  public function destroy(DestroyDiscountCodeRequest $objRequest, DiscountCode $objDiscountCode) {
+    Log::critical('Deleting an DiscountCode with ID as ' . $objDiscountCode->getIntId() . '.');
+
+    return redirect()
+      ->back()
+      ->with($objDiscountCode->delete() ? [
+        'stringSuccess' => 'Kortingscode succesvol verwijderd!',
+      ] : [
+        'stringError' => 'Kortingscode onsuccesvol verwijderd!',
+      ]);
+  }
+
+  /**
+   * @param EditDiscountCodeRequest $objRequest
+   * @param DiscountCode $objDiscountCode
+   */
+  public function edit(EditDiscountCodeRequest $objRequest, DiscountCode $objDiscountCode) {
+    Log::info('Editing an DiscountCode with ID as ' . $objDiscountCode->getIntId() . '.');
+
+    return view('discountcodes.edit')
+      ->with([
+        'objDiscountCode' => $objDiscountCode,
+      ]);
+  }
+
+  public function index() {
+    Log::info('Showing all the DiscountCodes.');
+
+    return view('discountcodes.index')
+      ->with([
+        'arrayDiscountCodes' => DiscountCode::all(),
+      ]);
+  }
+
+  /**
+   * @param ShowDiscountCodeRequest $objRequest
+   * @param DiscountCode $objDiscountCode
+   */
+  public function show(ShowDiscountCodeRequest $objRequest, DiscountCode $objDiscountCode) {
+    Log::info('Showing an DiscountCode with ID as ' . $objDiscountCode->getIntId() . '.');
+
+    return view('orders.show')
+      ->with([
+        'objDiscountCode' => $objDiscountCode,
+      ]);
+  }
+
+  /**
+   * @param StoreDiscountCodeRequest $objRequest
+   */
+  public function store(StoreDiscountCodeRequest $objRequest) {
+    Log::info('Storing a new DiscountCode.');
+
+    return redirect()
+      ->back()
+      ->with(DiscountCode::create($objRequest->all()) ? [
+        'stringSuccess' => 'KortingsCode succesvol aangemaakt!',
+      ] : [
+        'stringError' => 'KortingsCode onsuccesvol aangemaakt!',
+      ]);
+  }
+
+  /**
+   * @param UpdateDiscountCodeRequest $objRequest
+   * @param DiscountCode $objDiscountCode
+   */
+  public function update(UpdateDiscountCodeRequest $objRequest, DiscountCode $objDiscountCode) {
+    Log::info('Updating an DiscountCode with ID as ' . $objDiscountCode->getIntId() . '.');
+
+    return redirect()
+      ->back()
+      ->with($objDiscountCode->update($objRequest->all()) ? [
+        'stringSuccess' => 'KortingsCode succesvol aangepast!',
+      ] : [
+        'stringError' => 'KortingsCode onsuccesvol aangepast!',
+      ]);
+  }
+}
