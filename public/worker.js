24826c7a41a6b7249bfcf9b3c0edf57ce25a2a8a
Unknown <Jarnovogelzang@live.nl>
2018-07-16 15:48:00 +0200

    service worker

---
+++ public/worker.js
@@ -0,0 +1,36 @@
+self.addEventListener('install', function (objEvent) {
+  objEvent.waitUntil(caches.open('MilliSeconde').then(function (objCache) {
+    return objCache.addAll(['/']);
+  }));
+});
+
+self.addEventListener('fetch', function (objEvent) {
+  objEvent.respondWith(caches.match(objEvent.request).then(function (objResponse) {
+    if (objResponse) {
+      return objResponse;
+    }
+
+    var objRequestCloned = objEvent.request.clone();
+
+    return fetch(objRequestCloned).then(function (objResponse) {
+      if (!objResponse || objResponse.status !== 200 || objResponse.type !== 'basic') {
+        return objResponse;
+      }
+
+      var objResponseCloned = objResponse.clone();
+      caches.open('MilliSeconde').then(function (objCache) {
+        objCache.put(objEvent.request, objResponseCloned);
+      });
+
+      return objResponse;
+    });
+  }));
+});
+
+self.addEventListener('activate', function (objEvent) {
+  objEvent.waitUntil(caches.keys().then(function (arrayCacheNames) {
+    return Promise.all(arrayCacheNames.map(function (stringCacheName) {
+      return caches.delete(stringCacheName);
+    }));
+  }));
+});
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,25 +1,29 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="<?=str_replace('_', '-', app()->getLocale());?>">
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="csrf-token" content="<?=csrf_token();?>">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title><?=config('app.name', 'Laravel');?></title>
 
     <!-- Scripts -->
-    <script async defer src="{{ asset('js/app.js') }}"></script>
+    <script src="worker.js"></script>
+    <script async defer src="<?=asset('js/app.js');?>"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com" media="false" onload="if(media!='all')media='all'">
-    <link rel="shortcut icon" type="image/x-icon" href="<?=ENV('APP_URL');?>/favicon.ico">
+    <link rel="shortcut icon" type="image/x-icon" href="<?=ENV('APP_URL');?>/favicon.ico" media="false" onload="if(media!='all')media='all'">
     <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css" media="false" onload="if(media!='all')media='all'">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet" media="false" onload="if(media!='all')media='all'">
+    <link href="<?=asset('css/app.css');?>" rel="stylesheet" media="false" onload="if(media!='all')media='all'">
+
+    <!-- Manifests -->
+    <link rel="manifest" href="/manifest.json" media="false" onload="if(media!='all')media='all'">
 </head>
 <body>
     <div id="app">
