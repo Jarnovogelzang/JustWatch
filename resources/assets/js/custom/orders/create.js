247dafd62edf7661d94921c308dbed25b39b88f5
Unknown <Jarnovogelzang@live.nl>
2018-07-18 21:57:21 +0200

    nog meer dependencies

--- resources/assets/js/custom/orders/create.js
+++ resources/assets/js/custom/orders/create.js
@@ -1,5 +1,37 @@
 require('../bootstrap');
 
 $(document).ready(function () {
+  function getProducts() {
+    var objPromise = new Promise(function (callBackResolve, callBackReject) {
+      $.post('/AjaxController/fetchProducts', {
+        success: function (arrayProducts) {
+          callBackResolve(arrayProducts);
+        },
+        error: function (objError) {
+          callBackReject(objError);
+        }
+      });
+    });
 
+    return objPromise;
+  }
+
+  window.objIndexedDB.then(function (objDb) {
+    return objDb.transaction('store', 'readonly').objectStore('Product').getAll().loadArrayIntoJqueryObj();
+  }).then(function () {
+    return getProducts().then(function (arrayProducts) {
+      arrayProducts.loadArrayIntoJqueryObj();
+
+      return arrayProducts;
+    });
+  }).then(function (arrayProducts) {
+    return window.objIndexedDB.then(function (objDb) {
+      var objTransaction = objDB.transaction('store', 'readwrite');
+      objTransaction.objectStore('Product').put(arrayProducts);
+
+      return objTransaction.complete;
+    });
+  }).catch(function (objError) {
+    console.log('Something went wrong with the Error as ' + objError);
+  });
 });
--- resources/assets/js/custom/products/create.js
+++ resources/assets/js/custom/products/create.js
@@ -1,5 +1,37 @@
 require('../bootstrap');
 
 $(document).ready(function () {
+  function getCategories() {
+    var objPromise = new Promise(function (callBackResolve, callBackReject) {
+      $.post('/AjaxController/fetchCategories', {
+        success: function (arrayCategories) {
+          callBackResolve(arrayCategories);
+        },
+        error: function (objError) {
+          callBackReject(objError);
+        }
+      });
+    });
 
+    return objPromise;
+  }
+
+  window.objIndexedDB.then(function (objDb) {
+    return objDb.transaction('store', 'readonly').objectStore('Category').getAll().loadArrayIntoJqueryObj();
+  }).then(function () {
+    return getCategories().then(function (arrayCategories) {
+      arrayCategories.loadArrayIntoJqueryObj();
+
+      return arrayCategories;
+    });
+  }).then(function (arrayCategories) {
+    return window.objIndexedDB.then(function (objDb) {
+      var objTransaction = objDB.transaction('store', 'readwrite');
+      objTransaction.objectStore('Category').put(arrayCategories);
+
+      return objTransaction.complete;
+    });
+  }).catch(function (objError) {
+    console.log('Something went wrong with the Error as ' + objError);
+  });
 });
