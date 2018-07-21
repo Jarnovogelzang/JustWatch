3f50d32223ae1c159f86f0806664f032bc030f36
Unknown <Jarnovogelzang@live.nl>
2018-07-21 12:42:24 +0200

    nog meer axios

--- resources/assets/js/custom/products/create.js
+++ resources/assets/js/custom/products/create.js
@@ -10,22 +10,31 @@ document.addEventListener("DOMContentLoaded", function (objEvent) {
       });
   }
 
+  function getTags() {
+    return Axios.post('/fetchTags')
+      .then(function (objResult) {
+        return objResult;
+      }).catch(function (objError) {
+        return objError;
+      });
+  }
+
   window.objIndexedDB.then(function (objDb) {
-    return objDb.transaction('store', 'readonly').objectStore('Category').getAll().loadArrayIntoJqueryObj();
+    objDb.transaction('store', 'readonly').objectStore('Category').getAll().loadArrayIntoSelect();
+    objDb.transaction('store', 'readonly').objectStore('Tag').getAll().loadArrayIntoSelect();
   }).then(function () {
-    return getCategories().then(function (arrayCategories) {
-      arrayCategories.loadArrayIntoJqueryObj();
+    return Axios.all([getCategories(), getTags()])
+      .then(Axios.spread(function (arrayCategories, arrayTags) {
+        return window.objIndexedDB.then(function (objDb) {
+          var objTransaction = objDB.transaction('store', 'readwrite');
 
-      return arrayCategories;
-    });
-  }).then(function (arrayCategories) {
-    return window.objIndexedDB.then(function (objDb) {
-      var objTransaction = objDB.transaction('store', 'readwrite');
-      objTransaction.objectStore('Category').put(arrayCategories);
+          objTransaction.objectStore('Category').put(arrayCategories);
+          objTransaction.objectStore('Tag').put(arrayTags);
 
-      return objTransaction.complete;
-    });
+          return objTransaction.complete;
+        });
+      }));
   }).catch(function (objError) {
-    console.log('Something went wrong with the Error as ' + objError);
+    Toastr.error('Er is een fout opgetreden! Probeer opnieuw of neem contact op met ons!', 'Foutmelding!');
   });
 });
